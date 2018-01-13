7407c34d11875bf557f54af761b61ff34d4a5de8
saralyns <saralyns@uw.edu>
2018-01-13 11:17:04 -0800

    added auth.js

---
+++ js/auth.js
@@ -0,0 +1,93 @@
+$(function() {
+    
+    // Signs the user up for an account when the form is submitted
+    var signUp = function() {
+        // Get email, password, display name, and username
+        var email = $('#email').val();
+        var password = $("#password").val();
+        var displayName = $('#displayName').val();
+        // Get profile picture image
+        // Get the file
+        
+        var dataRef = firebase.database().ref('photos');
+        var storage = firebase.storage();
+        
+        var file = $("#fileUpload")[0].files[0];
+        // push file to database
+        // add
+        var fileRef = storage.ref(file.name);
+        
+        
+        console.log(file.name);
+        
+        
+        fileRef.put(file).then(function(){
+            fileRef.getDownloadURL().then(function(url) {
+                console.log(url);
+                var photo = url; 
+                
+                firebase.auth().createUserWithEmailAndPassword(email, password)
+                .then(function(user) {
+                    // Set display name and photo URL
+                    user.updateProfile({
+                        displayName: displayName,
+                        photoURL: photo
+                    }).then(function() {
+                        window.location = './';
+                    });
+                }).catch(function(error) {
+                    alert(error.message);
+                });
+            });
+        });
+    };
+    
+    // Signs the user into their Tweeter account
+    var signIn = function() {
+        // Get email and password
+        var email = $('#email').val();
+        var password = $("#password").val();
+
+        // Authenticate using email and password, then redirect
+        firebase.auth().signInWithEmailAndPassword(email, password).then(function() {
+            window.location = './';
+        }).catch(function(error) {
+            alert(error);
+        });
+    };
+
+    var signOut = function() {
+        firebase.auth().signOut().then(function() {
+            window.location = './sign-up.html';
+        });
+    };
+
+    $('form').on('submit', function(event) {
+        event.preventDefault();
+        var formId = $(this).attr('id');
+        if (formId == 'sign-up') {
+            signUp();
+        } else if (formId == 'sign-in') {
+            signIn();
+        }
+    });
+    
+    $('#logOut').on('click', function() {
+        signOut();
+    });
+
+    var checked;
+    firebase.auth().onAuthStateChanged(function(user) {
+        if (checked !== true) {
+            if (user && window.location.pathname.indexOf("sign") != -1) {
+                window.location = './';
+            }
+            if (!user && window.location.pathname.indexOf("sign") == -1) {
+                window.location = './sign-in.html';
+            }
+            checked = true;
+        }
+    });
+
+
+});
--- js/main.js
+++ js/main.js
@@ -1,14 +1,13 @@
+$(function() {
+   var config = {
+		 apiKey: "AIzaSyBK0cWiLuJayIuiSwikEL0BcGhyyl0xCn0",
+		 authDomain: "winfohackathon2018.firebaseapp.com",
+		 databaseURL: "https://winfohackathon2018.firebaseio.com",
+		 projectId: "winfohackathon2018",
+		 storageBucket: "",
+		 messagingSenderId: "2426079788"
+	  };
+	  firebase.initializeApp(config);
 
-<script src="https://www.gstatic.com/firebasejs/4.8.2/firebase.js"></script>
-<script>
-  // Initialize Firebase
-  var config = {
-    apiKey: "AIzaSyBK0cWiLuJayIuiSwikEL0BcGhyyl0xCn0",
-    authDomain: "winfohackathon2018.firebaseapp.com",
-    databaseURL: "https://winfohackathon2018.firebaseio.com",
-    projectId: "winfohackathon2018",
-    storageBucket: "",
-    messagingSenderId: "2426079788"
-  };
-  firebase.initializeApp(config);
-</script>
+
+});
