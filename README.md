5307b2bd15a0385929ea216c5e5742aa074b6a65
saralyns <saralyns@uw.edu>
2018-01-13 11:06:43 -0800

    added firebase, edited readme

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # WINFO-Hackathon-2018
+
+[website name goes here lol]
+Brandon Fok, Hunter Francia, Saralyn Santos, Sam Wheelock
---
+++ js/main.js
@@ -0,0 +1,14 @@
+
+<script src="https://www.gstatic.com/firebasejs/4.8.2/firebase.js"></script>
+<script>
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyBK0cWiLuJayIuiSwikEL0BcGhyyl0xCn0",
+    authDomain: "winfohackathon2018.firebaseapp.com",
+    databaseURL: "https://winfohackathon2018.firebaseio.com",
+    projectId: "winfohackathon2018",
+    storageBucket: "",
+    messagingSenderId: "2426079788"
+  };
+  firebase.initializeApp(config);
+</script>
