0a37f1561806f54ec3baf6000ee31551c89bebd4
bmoney123 <rangerred101@gmail.com>
2018-01-13 17:45:30 -0800

    blah blah blah 2394827394

---
+++ js/auth.js
@@ -0,0 +1,97 @@
+$(function() {
+
+	// Signs the user up for an account when the form is submitted
+	var signUp = function() {
+		// Get email, password, display name, and username
+		var email = $('#email').val();
+		var password = $("#password").val();
+		var displayName = $('#displayName').val();
+		
+		// Get profile picture image
+		// Get the file
+
+		var dataRef = firebase.database().ref('photos');
+		var storage = firebase.storage();
+
+		var file = $("#fileUpload")[0].files[0];
+		// push file to database
+		// add
+		var fileRef = storage.ref(file.name);
+
+
+		console.log(file.name);
+
+
+		fileRef.put(file).then(function(){
+			fileRef.getDownloadURL().then(function(url) {
+				console.log(url);
+				var photo = url; 
+
+				firebase.auth().createUserWithEmailAndPassword(email, password).then(function(user) {
+					// Set display name and photo URL
+					user.updateProfile({
+						displayName: displayName,
+						photoURL: photo
+					}).then(function() {
+						window.location = './index.html';
+					});
+				}).catch(function(error) {
+					alert(error.message);
+				});
+			});
+		});
+	};
+
+	// Signs the user into their account
+	var signIn = function() {
+		// Get email and password
+		var email = $('#email').val();
+		var password = $("#password").val();
+
+		// Authenticate using email and password, then redirect
+		firebase.auth().signInWithEmailAndPassword(email, password).then(function() {
+			window.location = './home_page.html';
+		}).catch(function(error) {
+			alert(error);
+		});
+	};
+
+	var signOut = function() {
+		firebase.auth().signOut().then(function() {
+			window.location = './signup.html';
+		});
+		
+	};
+
+	$('form').on('submit', function(event) {
+		event.preventDefault();
+		var formId = $(this).attr('id');
+		if (formId == 'signupForm') {
+			console.log("signing up...")
+			signUp();
+		} else if (formId == 'signinForm') {
+			console.log("signing in...")
+			signIn();
+		}
+	});
+
+	$('#logOut').on('click', function() {
+		signOut();
+	});
+
+	var checked;
+	firebase.auth().onAuthStateChanged(function(user) {
+		if (checked !== true) {
+			if (user && window.location.pathname.indexOf("sign") != -1 && window.location.pathname.indexOf("index") != -1) {
+				console.log("help");
+				window.location = './home_page.html';
+			}
+			if (!user && window.location.pathname.indexOf("sign") == -1 && window.location.pathname.indexOf("index") == -1) {
+				console.log("is this working???");
+				window.location = './index.html';
+			}
+			checked = true;
+		}
+	});
+
+});
---
+++ js/main.js
@@ -0,0 +1,78 @@
+$(function() {
+   var config = {
+		 apiKey: "AIzaSyBK0cWiLuJayIuiSwikEL0BcGhyyl0xCn0",
+		 authDomain: "winfohackathon2018.firebaseapp.com",
+		 databaseURL: "https://winfohackathon2018.firebaseio.com",
+		 projectId: "winfohackathon2018",
+		 storageBucket: "winfohackathon2018.appspot.com",
+		 messagingSenderId: "2426079788"
+	  };
+	  firebase.initializeApp(config);
+
+
+	/*	var favs = firebase.database().ref('favs');
+
+   favs.on('child_added', function(snapshot) {
+        var data = snapshot.val();
+        // Pass key, data to snapshot function
+        // renderFav(snapshot.key, data);
+    });
+	 
+    var renderTweet = function(id, data) {
+
+        var newTweet = $('<div>');
+        // append photo
+        var picture = $('<div>').addClass('img').css('background-image', 'url(' + data.photo + ')');
+        var dN = $('<span>').addClass('user').text(data.displayName);
+        dN.append('<br>');
+        var tweet = $('<span>').addClass("tweetText").text(data.tweet);
+
+        var likes = $('<div>');
+        var heart = $('<a href="#">');
+        heart.append('<i class="fa fa-heart" aria-hidden="true"></i>');
+        likes.append(heart);
+        likes.append(' | ');
+        var likesNumber = $('<span>').text('' + data.likes + ' likes').addClass('numberOfLikes');
+
+        likes.append(likesNumber);
+
+        heart.on('click', function() {
+            tweets.child(id).set({
+                displayName: data.displayName,
+                photo: data.photo,
+                tweet: data.tweet,
+                likes: (data.likes + 1)
+            });
+
+            newLikesNumber = $('<span>').text('' + (data.likes + 1) + ' likes');
+
+            likes.append(newLikesNumber);
+
+        });
+
+        // Append elements to the page
+        newTweet.append(picture);
+        newTweet.append(dN);
+        newTweet.append(tweet);
+        newTweet.append('<br>');
+        newTweet.append(likes);
+        newTweet.append('<hr>');
+        $('#allTweets').prepend(newTweet);
+    };
+     
+
+    $('#newFav').on('submit', function() {
+        favs.push({
+            displayName: firebase.auth().currentUser.displayName,
+            photo: firebase.auth().currentUser.photoURL,
+            tweet: $("#newTweet").val(),
+            likes: 0
+        });
+        $('#newTweet').val('');
+    }).then(function() {
+		console.log(favs);
+		window.location = '.favorites.html';
+	 });
+	 
+	 */
+});
